48dd01a7d51b5d114e97e6abe6459efdbaccadac
Siddique953 <siddiquec321@gmail.com>
2022-12-08 09:46:23 +0530

    My Orders , Profile & willPopScope

--- lib/Authentication/auth.dart
+++ lib/Authentication/auth.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:threems/Authentication/phoneUpdate.dart';
 import 'package:threems/Authentication/root.dart';
 import 'package:threems/pagess/onboardingpage.dart';
 
